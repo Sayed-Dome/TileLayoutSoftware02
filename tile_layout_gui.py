a4e0490c30863e5937a44c780346a2a49ac0c7c6
Sayed-Dome <sayed.alawi.dome@gmail.com>
2024-09-23 16:47:38 +0300

    Update tile_layout_gui.py
    4.47

--- tile_layout_gui.py
+++ tile_layout_gui.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem
+import json
+import csv
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsTextItem
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt
 from tile_layout import TileLayout
@@ -12,6 +14,24 @@ class TileLayoutGUI(QGraphicsView):
         self.scene = QGraphicsScene()
         self.setScene(self.scene)
         self.initUI()
+        self.selected_tile = None
+        self.dragging = False
+        self.resizing = False
+        self.resize_edge = None
+        self.undo_stack = []
+        self.redo_stack = []
+
+    def undo(self):
+        if self.undo_stack:
+            self.redo_stack.append(self.tile_layout.get_tiles())
+            self.tile_layout.set_tiles(self.undo_stack.pop())
+            self.drawTiles()
+
+    def redo(self):
+        if self.redo_stack:
+            self.undo_stack.append(self.tile_layout.get_tiles())
+            self.tile_layout.set_tiles(self.redo_stack.pop())
+            self.drawTiles()
 
     def initUI(self):
         self.setGeometry(300, 300, 300, 300)
@@ -36,22 +56,117 @@ class TileLayoutGUI(QGraphicsView):
             self._panStartY = event.y()
             self._panStartXValue = self.horizontalScrollBar().value()
             self._panStartYValue = self.verticalScrollBar().value()
+            self.selectTile(event)
+            self.dragging = True
+            self.checkResizeEdge(event)
+            self.undo_stack.append(self.tile_layout.get_tiles())
 
     def mouseMoveEvent(self, event):
         if event.buttons() == Qt.LeftButton:
-            x = event.x()
-            y = event.y()
-            self.horizontalScrollBar().setValue(self._panStartXValue - (x - self._panStartX))
-            self.verticalScrollBar().setValue(self._panStartYValue - (y - self._panStartY))
+            if self.dragging:
+                self.moveTile(event)
+            elif self.resizing:
+                self.resizeTile(event)
+            else:
+                x = event.x()
+                y = event.y()
+                self.horizontalScrollBar().setValue(self._panStartXValue - (x - self._panStartX))
+                self.verticalScrollBar().setValue(self._panStartYValue - (y - self._panStartY))
+                self.undo_stack.append(self.tile_layout.get_tiles())
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.setCursor(Qt.ArrowCursor)
+            self.dragging = False
+            self.resizing = False
+            self.undo_stack.append(self.tile_layout.get_tiles())
+
+    def selectTile(self, event):
+        pos = event.pos()
+        items = self.scene.items(pos)
+        if items:
+            self.selected_tile = items[0]
+
+    def moveTile(self, event):
+        if self.selected_tile:
+            pos = event.pos()
+            self.selected_tile.setPos(pos.x(), pos.y())
+            tile = self.getTileFromItem(self.selected_tile)
+            if tile:
+                tile.x = pos.x()
+                tile.y = pos.y()
+
+    def checkResizeEdge(self, event):
+        if self.selected_tile:
+            pos = event.pos()
+            tile = self.getTileFromItem(self.selected_tile)
+            if tile:
+                if abs(pos.x() - tile.x) < 5:
+                    self.resizing = True
+                    self.resize_edge = 'left'
+                elif abs(pos.x() - (tile.x + tile.width)) < 5:
+                    self.resizing = True
+                    self.resize_edge = 'right'
+                elif abs(pos.y() - tile.y) < 5:
+                    self.resizing = True
+                    self.resize_edge = 'top'
+                elif abs(pos.y() - (tile.y + tile.height)) < 5:
+                    self.resizing = True
+                    self.resize_edge = 'bottom'
+
+    def resizeTile(self, event):
+        if self.selected_tile:
+            pos = event.pos()
+            tile = self.getTileFromItem(self.selected_tile)
+            if tile:
+                if self.resize_edge == 'left':
+                    tile.width = tile.width - (pos.x() - tile.x)
+                    tile.x = pos.x()
+                elif self.resize_edge == 'right':
+                    tile.width = pos.x() - tile.x
+                elif self.resize_edge == 'top':
+                    tile.height = tile.height - (pos.y() - tile.y)
+                    tile.y = pos.y()
+                elif self.resize_edge == 'bottom':
+                    tile.height = pos.y() - tile.y
+                self.selected_tile.setRect(tile.x, tile.y, tile.width, tile.height)
+
+    def export_tile_layout_as_json(self):
+        tile_layout_data = {
+            'tiles': [
+                {
+                    'x': tile.x,
+                    'y': tile.y,
+                    'width': tile.width,
+                    'height': tile.height
+                }
+                for tile in self.tile_layout.get_tiles()
+            ]
+        }
+        with open('tile_layout.json', 'w') as f:
+            json.dump(tile_layout_data, f)
+
+    def export_tile_layout_as_csv(self):
+        tile_layout_data = [
+            [tile.x, tile.y, tile.width, tile.height]
+            for tile in self.tile_layout.get_tiles()
+        ]
+        with open('tile_layout.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['x', 'y', 'width', 'height'])
+            writer.writerows(tile_layout_data)
+
+    def getTileFromItem(self, item):
+        for tile in self.tile_layout.get_tiles():
+            if tile.x == item.x() and tile.y == item.y() and tile.width == item.boundingRect().width() and tile.height == item.boundingRect().height():
+                return tile
+        return None
+    
 
 def main():
     app = QApplication(sys.argv)
     tile_layout = TileLayout(10, 10)
-    tile1 = Tile(50, 50, 2, 2)
+    tile1 = Tile(0, 0, 2, 2)
     tile2 = Tile(2, 0, 2, 2)
     tile3 = Tile(4, 0, 2, 2)
     tile_layout.add_tile(tile1)
